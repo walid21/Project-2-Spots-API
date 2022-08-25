97d05acaa80ab970bba5d6c6c5ef7ce146cc77e7
Walid Jamili <wal.jam21@gmail.com>
2022-08-25 12:15:18 +0200

    Spot model

---
+++ models/Spot.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const spotSchema = new Schema({
+  name: {
+    type: Schema.Types.String,
+    // unique: true -> Ideally, should be unique, but its up to you
+  },
+  theme: {
+    type: Schema.Types.String,
+  },
+  activity: {
+    type: Schema.Types.String,
+  },
+  topography: {
+    type: Schema.Types.String,
+  },
+});
+
+const Spot = model("Spot", spotSchema);
+
+module.exports = Spot;
